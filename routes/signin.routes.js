0f0a627a155984afc5ff3c716ebb926e0a8bd26a
alikedo <alikemaldogan88@gmail.com>
2022-08-17 10:46:16 +0200

    done

---
+++ routes/signin.routes.js
@@ -0,0 +1,26 @@
+const async = require("hbs/lib/async");
+const User = require('../models/User.model');
+const router = require("express").Router();
+const bcrypt = require('bcryptjs');
+
+const { isLoggedOut } = require('../middleware/route-guard');
+
+
+/* GET home page */
+router.post("/signin", isLoggedOut, async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+        const resDB = await User.find({ username: username });
+        if (bcrypt.compareSync(password, resDB[0].password)) {
+            req.session.currentUser = resDB;
+            res.redirect("/home");
+        } else {
+            res.redirect("/?error=true");
+        }
+    } catch (err) {
+        console.log("Error while retrieving a user: ", err);
+        res.redirect("/?error=true");
+    }
+});
+
+module.exports = router;
---
+++ routes/signup.routes.js
@@ -0,0 +1,28 @@
+const async = require("hbs/lib/async");
+const User = require('../models/User.model');
+const router = require("express").Router();
+const bcrypt = require('bcryptjs');
+
+const { isLoggedOut } = require('../middleware/route-guard');
+
+/* GET home page */
+router.get("/signup", isLoggedOut, (req, res, next) => {
+    res.render("auth/signup");
+});
+
+router.post("/signup", isLoggedOut, async (req, res, next) => {
+    const { username, password } = req.body;
+
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        hash = await bcrypt.hashSync(password, salt);
+        const resDB = await User.create({ username, password: hash });
+        console.log(resDB);
+        res.redirect("/");
+    } catch (err) {
+        console.log("Error while creating a user: ", err);
+        res.redirect("/signup?error=true");
+    }
+});
+
+module.exports = router;
